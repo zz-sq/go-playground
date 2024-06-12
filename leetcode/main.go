6d2d19bb3f9df6fc6ee8a940d6868e55da7682bc
zhangshiqian.2020 <zhangshiqian.2020@bytedance.com>
2024-06-12 18:32:01 +0800

    0612

--- leetcode/main.go
+++ leetcode/main.go
@@ -1,28 +1,19 @@
 package main
 
 import (
-	"fmt"
+	"slices"
 )
 
 // 题集：https://leetcode.cn/studyplan/top-interview-150/
 // https://leetcode.cn/problems/remove-element/description/?envType=study-plan-v2&envId=top-interview-150
-func majorityElement(nums []int) int {
-	votes, res := 0, 0
-	for _, v := range nums {
-		if votes == 0 {
-			res = v
-		}
-		if res == v {
-			votes++
-		} else {
-			votes--
-		}
-	}
-	return res
+func rotate(nums []int, k int) {
+	k %= len(nums)
+	slices.Reverse(nums)
+	slices.Reverse(nums[:k])
+	slices.Reverse(nums[k:])
 }
 
 func main() {
-	nums1 := []int{1, 1, 1, 1, 2, 3}
-	fmt.Println(majorityElement(nums1))
-	fmt.Println(nums1)
+	nums1 := []int{1, 2, 3, 4, 5, 6, 7}
+	rotate(nums1, 3)
 }
