e27315e3c92e6b8993287c95465454ade5d3ac64
zhangshiqian.2020 <zhangshiqian.2020@bytedance.com>
2024-06-19 19:13:45 +0800

    0619

--- leetcode/main.go
+++ leetcode/main.go
@@ -3,28 +3,30 @@ package main
 import "fmt"
 
 // 题集：https://leetcode.cn/studyplan/top-interview-150/
-func productExceptSelf(nums []int) []int {
-	tmp, zero := 1, 0
-	for _, num := range nums {
-		if num == 0 {
-			zero += 1
+func canCompleteCircuit(gas []int, cost []int) int {
+	for i, n := 0, len(gas); i < n; {
+		sumGas, sumCost, j := 0, 0, 0
+		for j < n {
+			k := (i + j) % n
+			sumGas += gas[k]
+			sumCost += cost[k]
+			if sumCost > sumGas {
+				break
+			}
+			j++
+		}
+		if j == n {
+			return i
 		} else {
-			tmp *= num
+			i += j + 1
 		}
 	}
-	ans := make([]int, len(nums))
-	for i, num := range nums {
-		if zero == 1 && num == 0 {
-			ans[i] = num
-		} else if zero > 1 {
-			ans[i] = 0
-		} else {
-			ans[i] = tmp / num
-		}
-	}
-	return ans
+	return -1
 }
 
+// 1 2 3 4 5
+// 3 4 5 1 2
+// -2 -2 -2 4 3
 func main() {
-	fmt.Println(productExceptSelf([]int{1, 2, 3, 4, 5}))
+	fmt.Println(canCompleteCircuit([]int{1, 2, 3, 4, 5}, []int{3, 4, 5, 1, 2}))
 }
