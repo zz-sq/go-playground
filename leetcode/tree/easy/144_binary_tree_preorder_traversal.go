5bde0274db3c0d8e101941d8dc858bfaf49431e9
zhangshiqian.2020 <zhangshiqian.2020@bytedance.com>
2024-07-16 16:42:37 +0800

    0716

---
+++ leetcode/tree/easy/144_binary_tree_preorder_traversal.go
@@ -0,0 +1,79 @@
+/*
+ * @lc app=leetcode.cn id=144 lang=golang
+ *
+ * [144] Binary Tree Preorder Traversal
+ *
+ * https://leetcode.cn/problems/binary-tree-preorder-traversal/description/
+ *
+ * algorithms
+ * Easy (72.01%)
+ * Likes:    1258
+ * Dislikes: 0
+ * Total Accepted:    1.1M
+ * Total Submissions: 1.5M
+ * Testcase Example:  '[1,null,2,3]'
+ *
+ * Given the root of a binary tree, return the preorder traversal of its nodes'
+ * values.
+ *
+ *
+ * Example 1:
+ *
+ *
+ * Input: root = [1,null,2,3]
+ * Output: [1,2,3]
+ *
+ *
+ * Example 2:
+ *
+ *
+ * Input: root = []
+ * Output: []
+ *
+ *
+ * Example 3:
+ *
+ *
+ * Input: root = [1]
+ * Output: [1]
+ *
+ *
+ *
+ * Constraints:
+ *
+ *
+ * The number of nodes in the tree is in the range [0, 100].
+ * -100 <= Node.val <= 100
+ *
+ *
+ *
+ * Follow up: Recursive solution is trivial, could you do it iteratively?
+ *
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func preorderTraversal(root *TreeNode) []int {
+	res := []int{}
+	var dfs func(*TreeNode)
+	dfs = func(node *TreeNode) {
+		if node == nil {
+			return
+		}
+		res = append(res, node.Val)
+		dfs(node.Left)
+		dfs(node.Right)
+	}
+	dfs(root)
+	return res
+}
+
+// @lc code=end
+
---
+++ leetcode/tree/easy/145_binary_tree_postorder_traversal.go
@@ -0,0 +1,78 @@
+/*
+ * @lc app=leetcode.cn id=145 lang=golang
+ *
+ * [145] Binary Tree Postorder Traversal
+ *
+ * https://leetcode.cn/problems/binary-tree-postorder-traversal/description/
+ *
+ * algorithms
+ * Easy (76.70%)
+ * Likes:    1184
+ * Dislikes: 0
+ * Total Accepted:    799.6K
+ * Total Submissions: 1M
+ * Testcase Example:  '[1,null,2,3]'
+ *
+ * Given the root of a binary tree, return the postorder traversal of its
+ * nodes' values.
+ *
+ *
+ * Example 1:
+ *
+ *
+ * Input: root = [1,null,2,3]
+ * Output: [3,2,1]
+ *
+ *
+ * Example 2:
+ *
+ *
+ * Input: root = []
+ * Output: []
+ *
+ *
+ * Example 3:
+ *
+ *
+ * Input: root = [1]
+ * Output: [1]
+ *
+ *
+ *
+ * Constraints:
+ *
+ *
+ * The number of the nodes in the tree is in the range [0, 100].
+ * -100 <= Node.val <= 100
+ *
+ *
+ *
+ * Follow up: Recursive solution is trivial, could you do it iteratively?
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func postorderTraversal(root *TreeNode) []int {
+	res := []int{}
+	var dfs func(*TreeNode)
+	dfs = func(node *TreeNode) {
+		if node == nil {
+			return
+		}
+		dfs(node.Left)
+		dfs(node.Right)
+		res = append(res, node.Val)
+	}
+	dfs(root)
+	return res
+}
+
+// @lc code=end
+
